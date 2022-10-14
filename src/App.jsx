b460b9e20c1837052c88c679b5d92da92fa1f37d
Mrityunjay <68852546+MrityunjayM@users.noreply.github.com>
2022-10-14 20:11:43 +0530

    added task delete btn with functionality (#2)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { nanoid } from 'nanoid';
+import { nanoid } from "nanoid";
 
 import Task from "./comps/Task";
 import "./App.css";
@@ -8,17 +8,29 @@ function App() {
   const [taskList, setTaskList] = useState([]);
 
   const addTaskHandler = () => {
-    let newTask = { taskId: nanoid(8), title: "First Task", text: "first task in the list to workout"}
-    setTaskList(prevState => ([...prevState, newTask]));
+    let newTask = {
+      taskId: nanoid(8),
+      title: "First Task",
+      text: "first task in the list to workout",
+    };
+    setTaskList((prevState) => [...prevState, newTask]);
     return;
-  }
+  };
+
+  const deleteTaskHandler = (taskId) => {
+    setTaskList((prevState) => prevState.filter((t) => t.taskId !== taskId));
+  };
 
   return (
     <div className="App">
       <h1>Learning Github with React+Vite App Example.</h1>
       <div className="card">
-        <button className="btn-add-task" onClick={addTaskHandler}> <b>+</b> Task</button>
-        {taskList?.map(task => (<Task key={task.taskId} title={task.title} text={task.text}/>))}
+        <button className="btn-add-task" onClick={addTaskHandler}>
+          <b>+</b> Task
+        </button>
+        {taskList?.map((task) => (
+          <Task key={task.taskId} taskId={task.taskId} title={task.title} text={task.text} onDelete={deleteTaskHandler} />
+        ))}
       </div>
     </div>
   );
