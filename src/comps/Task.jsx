28702fd39ff3de4b914b5c75fb90681318b4aa16
Mrityunjay Mishra <mrityunjaymishra@hotmail.com>
2022-10-14 19:52:40 +0530

    added task delete btn with functionality

--- src/comps/Task.jsx
+++ src/comps/Task.jsx
@@ -1,8 +1,8 @@
-const Task = ({ title, text }) => {
+const Task = ({ taskId, title, text, onDelete }) => {
   return (
     <div className="card">
       <h3>{title}</h3>
-      <p>{text}</p>
+      <p>{text} <button className="btn-del-task" onClick={() => onDelete(taskId)}>X</button></p>
     </div>
   );
 };
